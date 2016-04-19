e723762f67cd88d4eb98ae23a9bd223ff906591b
techcamptoyama <el.scorcho.weezer@gmail.com>
2016-04-19 22:56:39 +0900

    add edit and delete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
     @projects = Project.includes(:company).page(params[:page]).per(5).order("created_at DESC")
   end
@@ -6,12 +8,35 @@ class ProjectsController < ApplicationController
   def new
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+    project = Project.find(params[:id])
+    if project.company_id = current_company.id
+      project.destroy
+    end
+  end
+
+  def update
+    project = Project.find(params[:id])
+    if project.company_id = current_company.id
+    project.update(project_params)
+    end
+  end
+
   def create
     Project.create(title: project_params[:title], project_detail: project_params[:project_detail], industry: project_params[:industry], other: project_params[:other], company_id: current_company.id)
   end
+
     private
   def project_params
     params.permit(:title, :project_detail, :industry, :other)
+  end
+
+  def move_to_index
+      redirect_to action: :index unless company_signed_in?
   end
 
 end
