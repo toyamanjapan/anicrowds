2ffb3628e54a2385603d817bf73cc284ce4a216a
techcamptoyama <el.scorcho.weezer@gmail.com>
2016-04-20 22:45:21 +0900

    5th commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420105203) do
+ActiveRecord::Schema.define(version: 20160420133002) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160420105203) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.integer  "company_id",     limit: 4
+    t.text     "image",          limit: 65535
   end
 
   create_table "users", force: :cascade do |t|
