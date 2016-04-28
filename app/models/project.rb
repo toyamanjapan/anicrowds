1af747be15f8e8f5a407f3f93b4953be02b1edf7
techcamptoyama <el.scorcho.weezer@gmail.com>
2016-04-28 21:38:28 +0900

    add deadline to master

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,10 @@ class Project < ActiveRecord::Base
   belongs_to :user
   belongs_to :company
   has_many :concepts
+
+def count
+  difference = (self.created_at + 7.days).to_date - Time.now.to_date
+  difference.to_i
+end
+
 end
