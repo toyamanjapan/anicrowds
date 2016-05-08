678457071c7701b539261bef80332d1adf158582
toyamanjapan <yuya.toyama@8ce.me>
2016-05-08 19:58:08 +0900

    modify us-cdbr-iron-east-03.cleardb.net in config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'us-cdbr-iron-east-03.cleardb.net' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   config.action_mailer.smtp_settings = {
   :enable_starttls_auto => true,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'nameless-brook-91623.herokuapp.com' }
+
 end
